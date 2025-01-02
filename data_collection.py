5478d3df56d59256e9edbc0b8fd2cbdde43074d4
Amit Duwal <amit.duwal@cubit.com.np>
2025-01-02 10:14:00 +0545

    make changes to mine all data

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,7 @@
 import requests as req
 import pandas as pd
 import os
+import time
 
 
 def get_commodity_price_data(commodity_id, from_date, to_date):
@@ -16,11 +17,12 @@ for commodity_id, commodity_name in zip(
     commodity_list_df["commodity_id"], commodity_list_df["commodity_name"]
 ):
     print(commodity_id, commodity_name)
+    time.sleep(2)
     if isinstance(commodity_id, float) and commodity_id.is_integer():
         commodity_id = int(commodity_id)
     commodity_price = get_commodity_price_data(
         commodity_id,
-        "2015-1-1",
+        "2013-1-1",
         "2024-12-25",
     )
     commodity_price_df = pd.DataFrame(commodity_price)
